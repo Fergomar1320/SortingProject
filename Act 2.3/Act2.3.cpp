341f14db2d9324d8c3b7a70317f46f3987c61db4
NolbertoCastro <nolber0703@gmail.com>
2022-10-08 09:28:31 -0500

    Modifications to the file reading function

--- Act 2.3/Act2.3.cpp
+++ Act 2.3/Act2.3.cpp
@@ -49,7 +49,6 @@ template <class T> class Node{
         }//Complexity O(1)
 };
 
-
 template<class T> class doubleLinkedList{
     private:
         Node<T> *head;
@@ -104,7 +103,7 @@ template<class T> class doubleLinkedList{
         } //Complexity O(n)
 };
 
-void fileReading(doubleLinkedList<string> &DoubleList){
+void fileReading(vector <string> &data, vector <double> &hashvalues){
     ifstream dataBase;
     dataBase.open("database.txt");
     string month, day, time, error, otherChar, completeLine;
@@ -115,69 +114,98 @@ void fileReading(doubleLinkedList<string> &DoubleList){
             //Reading the complete file and saving data in variables;
             dataBase >> month >> day >> time >> oneN >> sillychar >> twoN >> sillychar >> threeN >> sillychar >> fourN >> sillychar >> fiveN;
             getline(dataBase, error); 
-            int hashValue = oneN + twoN + threeN + fourN + fiveN;
-            completeLine = month + " " + day + " " + time + " " + to_string(oneN) + "." + to_string(twoN) + "." + to_string(threeN) + "." + to_string(fourN) + ":" + to_string(fiveN) + error;
-            DoubleList.addToList(completeLine, hashValue);
+            double hashValue = static_cast<double>(oneN) + (static_cast<double>(twoN)/100) + (static_cast<double>(threeN)/100000) + (static_cast<double>(fourN)/100000000); //+ fiveN;
+            completeLine = month + " " + day + " " + time + " " + to_string(oneN) + "." + to_string(twoN) + "." + to_string(threeN) + "." 
+            + to_string(fourN) + ":" + to_string(fiveN) + error + to_string(hashValue);
+            data.push_back(completeLine);
+            hashvalues.push_back(hashValue);
+            //DoubleList.addToList(completeLine, hashValue);
         }
         dataBase.close();
     }
     cout << "El archivo se leyó correctamente" << endl;
 } //Time Complexity O(n)
 
+void writeFile(vector <string> &data, vector <double> &hashvalues){
+    fstream dataBase;
+    dataBase.open("sortData.txt",ios::out);
+    if(dataBase.is_open()){
+        for(int i = 1;i <= data.size(); i++){
+            dataBase << data[i] << endl;
+        }
+        dataBase.close();
+    }
+} //Time Complexity O(n^2)
 
-// void mergeOrder(doubleLinkedList<string> &DoubleList, int start, int half, int end){
-//     vector<double> vI;
-//     vector<double> vD;
+void mergeOrder(vector<double> &hashvalues, vector<string> &data, int start, int half, int end){
+    vector<double> vI;
+    vector<double> vD;
+    vector<string> sI;
+    vector<string> sD;
 
-//     int numIzq = half - start + 1;
-//     int numDer = end - half;
+    int numIzq = half - start + 1;
+    int numDer = end - half;
 
-//     for(int i = 0; i < numIzq; i++){
-//      vI.push_back(dates[start + i]);
-//     }
-//     for(int i = 0; i < numDer; i++){
-//         vD.push_back(dates[half + 1 + i]);
-//     }
+    for(int i = 0; i < numIzq; i++){
+     vI.push_back(hashvalues[start + i]);
+     sI.push_back(data[start + i]);
+    }
+    for(int i = 0; i < numDer; i++){
+        vD.push_back(hashvalues[half + 1 + i]);
+        sD.push_back(data[half + 1 + i]);
+    }
 
-//     double i = 0;
-//     double j = 0; 
-//     double k = start;
+    double i = 0;
+    double j = 0; 
+    double k = start;
 
-//     while(i < numIzq && j < numDer){
-//         if (vI[i] <= vD[j]){
-//             dates[k] = vI[i];
-//             i++;
-//         } else{
-//             dates[k] = vD[j];
-//             j++;
-//         }
-//         k++;
-//     }
+    while(i < numIzq && j < numDer){
+        if (vI[i] <= vD[j]){
+            hashvalues[k] = vI[i];
+            data[k] = sI[i];
+            i++;
+        } else{
+            hashvalues[k] = vD[j];
+            data[k] = sD[j];
+            j++;
+        }
+        k++;
+    }
 
-//     while(j < numDer){
-//         dates[k] = vD[j];
-//         j++;
-//         k++;
-//     }
-//     while(i < numIzq){
-//         dates[k] = vI[i];
-//         i++;
-//         k++;
-//     }
-// } //Time Complexity O(n log n)
+    while(j < numDer){
+        hashvalues[k] = vD[j];
+        data[k] = sD[j];
+        j++;
+        k++;
+    }
+    while(i < numIzq){
+        hashvalues[k] = vI[i];
+        data[k] = sI[i];
+        i++;
+        k++;
+    }
+} //Time Complexity O(n log n)
 
-// void mergeSort(doubleLinkedList<string> &DoubleList, string cuantity, int start, int end){
-//     if(start < end){
-//         int half = start + (end - start) / 2;
-//         mergeSort(DoubleList, data, start, half);
-//         mergeSort(DoubleList, data, half + 1, end);
-//         mergeOrder(DoubleList, start, half, end);
-//     }
-// } 
+void mergeSort(vector<double> &hashvalues, vector<string> &data, int start, int end){
+    if(start < end){
+        int half = start + (end - start) / 2;
+        mergeSort(hashvalues, data, start, half);
+        mergeSort(hashvalues, data, half + 1, end);
+        mergeOrder(hashvalues, data, start, half, end);
+    }
+} 
 
 int main(){
+    vector <string> data;
+    vector <double> hashvalues;
     doubleLinkedList<string> firstList;
-    fileReading(firstList);
+
+    cout << "The file lecture has begun" << endl;
+    fileReading(data, hashvalues);
+    cout << "Organizing......" << endl;
+    mergeSort(hashvalues, data, 0, hashvalues.size()-1);
+    cout << "The sorting has been completed" << endl;
+    writeFile(data, hashvalues);
     firstList.printList();
     return 0;
 }
